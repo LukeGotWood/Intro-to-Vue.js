e9e4955a38c30616153347bb10f116dd0e0953ab
LukeGotWood <l.wood0097@gmail.com>
2020-07-27 20:39:23 +0100

    10. Forms

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,76 @@
 'use strict';
 
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+
+            <p>
+                <label for="name">Name:</label>
+                <input id="name" v-model="name"></input>
+            </p>
+
+            <p>
+                <label for="review">Review:</label>
+                <textarea id="review" v-model="review"></textarea>
+            </p>
+
+            <p>
+                <label for="rating">Rating:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+
+            <p>
+                <input type="submit" value="Submit">
+            </p>
+        </form>
+        `,
+
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+
+    methods: {
+        onSubmit: function() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                };
+    
+                this.$emit('review-submitted', productReview);
+    
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+            } else {
+                if (!this.name) this.errors.push("Name required!");
+                if (!this.review) this.errors.push("Review required!");
+                if (!this.rating) this.errors.push("Rating required!");
+            }
+        }
+    }
+});
+
 Vue.component('product', {
     props: {
         premium: {
@@ -8,34 +79,50 @@ Vue.component('product', {
         }
     },
     template: `
-    <div class="product">
-        <div class="product-image">
-            <img :src="image">
-        </div>
-        <div class="product-info">
-            <h1>{{ title }}</h1>
-            <span v-if="onSale">On Sale!</span>
-            <p v-if="inStock">In Stock</p>
-            <p v-else>Out of Stock</p>
-            <p>Shipping: {{ shipping }}</p>
-            <p>{{ description }}</p>
+        <div class="product">
+            <div class="product-image">
+                <img :src="image">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <span v-if="onSale">On Sale!</span>
+                <p v-if="inStock">In Stock</p>
+                <p v-else>Out of Stock</p>
+                <p>Shipping: {{ shipping }}</p>
+                <p>{{ description }}</p>
 
-            <ul>
-                <li v-for="detail in details">{{ detail }}</li>
-            </ul>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
 
-            <p>Sizes:</p>
-            <ul>
-                <li v-for="size in sizes">{{ size }}</li>
-            </ul>
+                <p>Sizes:</p>
+                <ul>
+                    <li v-for="size in sizes">{{ size }}</li>
+                </ul>
 
-            <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box" :style="{ backgroundColor: variant.variantColor}" @mouseover="updateProduct(index)">
-            
+                <div v-for="(variant, index) in variants" :key="variant.variantId" class="color-box" :style="{ backgroundColor: variant.variantColor}" @mouseover="updateProduct(index)">
+                
+                </div>
+
+                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
             </div>
 
-            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to Cart</button>
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted="addReview"></product-review>
+
         </div>
-    </div>`,
+        `,
 
     data: function() {
         return {
@@ -60,7 +147,9 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
-            sizes: ['Small', 'Medium', 'Large', 'X-Large']}
+            sizes: ['Small', 'Medium', 'Large', 'X-Large'],
+            reviews: []
+        }
     },
 
     methods: {
@@ -70,6 +159,10 @@ Vue.component('product', {
 
         updateProduct: function(index) {
             this.selectedVariant = index;
+        },
+
+        addReview: function(productReview) {
+            this.reviews.push(productReview);
         }
     },
 
